beac75f744db76d01fb111cd1a35b50b29a08b16
unknown <ARM-1@ARM1.cenki.ru>
2019-07-31 11:56:47 +0100

    tasks settings have been changed and added tasks fonts, clean

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,50 @@ var gulp          = require('gulp'),
     cleanCss      = require('gulp-clean-css'),      // *gulp-clean-css (для минификации css)
     autoprefixer  = require('gulp-autoprefixer'),   // *gulp-autoprefixer (для расстановки префиксов)
     imagemin      = require('gulp-imagemin'),       // *gulp-imagemin (для минификации изображений)
+    cache         = require('gulp-cache'),          // *gulp-cache (для кеширования данных)
     del           = require('del');                 // *del (для удаления файлов и папок)
-
-    
+   
 var PATHS = {
   src: 'src',
   dist: 'dist'
 }
 
 
-// add browser-sync
-// add clean 'dist'
+/**
+    * LOCAL SERVER
+    * 1. создание таска 'browser-sync'
+    * 2. директория для сервера 
+    * 3. отключение всплывающих уведомлений в браузере
+ */
+
+gulp.task('browser-sync', function () {
+  browserSync({
+    server: {
+      baseDir: `${PATHS.dist}`
+    },
+    notify: false
+  })
+})
 
 
 /**
-    * HTML
-    * 1. создание таска 'html'
-    * 2. исходная директория (all files.html)
-    * 3. конечная директория
+    * SCRIPTS
+    * 1. создание таска 'scripts'
+    * 2. исходная директория (all files.js)
+    * 3. конкатенация files.js -> script.min.js
+    * 4. минификация файла script.min.js
+    * 5. выгрузка файла script.min.js в 'dist/js'
  */
 
-gulp.task('html', function() {
-  return gulp.src(`${PATHS.src}/*.html`)
-    .pipe(gulp.dest(`${PATHS.dist}`))
+gulp.task('scripts', function () {
+  return gulp.src([
+    `node_modules/jquery/dist/jquery.min.js`,
+    `${PATHS.src}/scripts/*.js`,
+  ])
+    .pipe(concat('script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(`${PATHS.dist}/js`))
+    .pipe(browserSync.reload({ stream: true }))
 })
 
 
@@ -43,7 +64,7 @@ gulp.task('html', function() {
     * 7. выгрузка файла style.min.css в 'dist/css'
  */
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src(`${PATHS.src}/styles/**/*.sass`)
     .pipe(concat('style.min.sass'))
     .pipe(sass())
@@ -53,41 +74,56 @@ gulp.task('styles', function() {
     }))
     .pipe(cleanCss())
     .pipe(gulp.dest(`${PATHS.dist}/css`))
+    .pipe(browserSync.reload({ stream: true }))
 })
 
 
 /**
-    * SCRIPTS
-    * 1. создание таска 'scripts'
-    * 2. исходная директория (all files.js)
-    * 3. конкатенация files.js -> script.min.js
-    * 4. минификация файла script.min.js
-    * 5. выгрузка файла script.min.js в 'dist/js'
+    * HTML
+    * выгрузка html в 'dist'
  */
 
- gulp.task('scripts', function() {
-   return gulp.src([
-      //  'node_modules/'
-      `${PATHS.src}/scripts/*.js`,
-    ])
-    .pipe(concat('script.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest(`${PATHS.dist}/js`))
- })
+gulp.task('html', function () {
+  return gulp.src(`${PATHS.src}/*.html`)
+    .pipe(gulp.dest(`${PATHS.dist}`))
+    .pipe(browserSync.reload({ stream: true }))
+})
 
 
 /**
    * IMAGES
-   * 1. создание таска 'images'
-   * 2. исходная директория (all images)
-   * 3. минификация all images
-   * 4. выгрузка all images в 'dist/img'
+   * кеширование, минификация и выгрузка изображений в 'dist'
 */
 
-gulp.task('images', function() {
-  return gulp.src(`${PATHS.src}/images/**/*.{png,jpg,jpeg,gif}`)
-    .pipe(imagemin())
+gulp.task('images', function () {
+  return gulp.src(`${PATHS.src}/images/**/*.{png,jpg,jpeg,gif,ico}`)
+    .pipe(cache(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 3 })
+    ])))
     .pipe(gulp.dest(`${PATHS.dist}/img`))
+})
+
+
+/**
+   * FONTS
+   * выгрузка шрифтов в 'dist'
+*/
+
+gulp.task('fonts', function () {
+  return gulp.src(`${PATHS.src}/fonts/**/*`)
+    .pipe(gulp.dest(`${PATHS.dist}/fonts`))
+})
+
+
+/**
+   * CLEAN
+   * удаление директории 'dist'
+*/
+
+gulp.task('clean', function () {
+  return del.sync(`${PATHS.dist}`)
 })
 
 
@@ -96,11 +132,11 @@ gulp.task('images', function() {
    * наблюдение за изменениями в файлах
 */
 
-gulp.task('watch', function() {
-  gulp.watch('src/*.html', gulp.parallel('html'));
-  // gulp.watch('src/styles/**/*.sass', gulp.parallel('styles'));
-  gulp.watch('src/scripts/**/*.sass', gulp.parallel('scripts'));
-  gulp.watch('src/images/**/*', gulp.parallel('images'));
+gulp.task('watch', function () {
+  gulp.watch(`${PATHS.src}/*.html`, gulp.parallel('html'));
+  gulp.watch(`${PATHS.src}/styles/**/*.sass`, gulp.parallel('styles'));
+  gulp.watch(`${PATHS.src}/scripts/**/*.js`, gulp.parallel('scripts'));
+  gulp.watch(`${PATHS.src}/images/**/*`, gulp.parallel('images'));
 })
 
 
@@ -109,4 +145,5 @@ gulp.task('watch', function() {
    * запуск тасков по умолчанию 
 */
 
-gulp.task('default', gulp.parallel('html', 'styles', 'scripts', 'images', 'watch'))
+gulp.task('default', gulp.parallel('clean', 'html', 'styles', 'scripts', 'images', 'fonts', 'browser-sync', 'watch'))
+
